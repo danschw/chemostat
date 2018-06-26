4178fb3e9f9c2f7d69f02db86c41ca6c10070b22
@danschw <danschw@iu.edu>
2018-06-26 12:41:34 -0400

    Added Sporulation in 3 stages

--- code/chemostat_ode.R
+++ code/chemostat_ode.R
@@ -7,7 +7,7 @@ rm(list=ls())
 library(deSolve)
 library(ggplot2)
 
-## Starting off without sporulation
+## Model with multi stage sporulation
 #Define system of differential equations for resource and bacteria
 chemostat <- function(t,current,params){
   with(as.list(c(params,current)), {
@@ -16,33 +16,47 @@ chemostat <- function(t,current,params){
     dR <- D * (R_in - R) - mu * R * N
     
     #N are the bacteria 
-    dN <- mu * R * N - D * N
+    dN <- mu * R * N - D * N - prob_spor * N + prob_actv * Sp3
+    
+    # Sporulation takes 3 time steps. modeled as 3 population maturig through the process
+    dSp1 <- prob_spor * N - Sp1
+    
+    dSp2 <- Sp1 - Sp2
+    
+    dSp3 <- Sp2 - prob_actv * Sp3
+    
 
-    results <- c(dR, dN)
+    results <- c(dR, dN, dSp1, dSp1, dSp3)
     return (list(results))
   })
 }
 
 
 
+###define parameters
+# resource in inflow
+R_in <- 1000
+#dilution rate
+D <- 2
+# growth rate
+mu <- 2
+# probaility of sporulation
+prob_spor <- 0.001
+#probability of reactivation of spores
+prob_actv <- 0.001
 
+# all parameters together
+params <- c(R_in, D, mu, prob_spor, prob_actv)
 
 #declare initial conditions for state variables
-current0 <- c(R = R_in, N = 10)
-
-###declare parameters
-# resource in inflow
-R_in <- 50
-
-# Dilution rate
-params <- c(D = 0.5, mu = 3)
+current0 <- c(R = R_in, N = 100, Sp1=0, Sp2=0, Sp3=0)
 
 #define the time to run model
-t <- seq (0, 1000, by=0.1)
+t <- seq (0, 5000, by=0.1)
 
 #solve the model and save the output
 out <- lsoda(current0, t, chemostat, params)
 
-plot (out, log='y')
+plot (out)
 
 
